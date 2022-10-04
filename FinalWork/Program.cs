0a121f66b3ac5860ebd2149fdbe5f924c2a58c24
BoginskiyVV <vov4ik84@gmail.com>
2022-10-05 01:13:47 +0300

    add solve task

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -2,3 +2,30 @@
 // которых меньше, либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма.
 
+string[] array1 = new string[] { "1", "22", "333", "в", "4444", "dddd" };
+string[] array2 = new string[array1.Length];
+
+CreateArray(array1, array2);
+PrintArray(array2);
+
+void CreateArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
